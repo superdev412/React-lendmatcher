05025564480dbb292b0c6b29300434f56b08fee0
Super-Smile <baydev1998@gmail.com>
2022-02-28 14:59:53 +0300

    first commit

---
+++ src/components/dashboard/About.js
@@ -0,0 +1,16 @@
+import '../../assets/css/dashboard/About.css';
+import check from '../../assets/img/check.png';
+import nba from '../../assets/img/NBA.png';
+import jordan from '../../assets/img/jordan.png';
+import kSwiss from '../../assets/img/k-swiss.png';
+import puma from '../../assets/img/puma.png';
+
+function About() {
+  return(
+    <div id="about" className="content-about">
+      sdgsdfsdfsdf
+    </div>
+  )
+}
+
+export default About;
---
+++ src/components/dashboard/Metamorph.js
@@ -0,0 +1,15 @@
+import '../../assets/css/dashboard/Metamorph.css';
+import nft from '../../assets/img/nft1.png'
+
+function Metamorph() {
+  return(
+    <div id="meta" className="content-meta">
+      <div>
+        <p className="logo-word">LENDMATCHER</p>
+        <p className="logo-description">LENDMATCHER IS WHERE ENTREPRENEURS WITH IDEAS MEET PRIVATE INVESTORS  !!!</p>
+      </div>
+    </div>
+  )
+}
+
+export default Metamorph;
